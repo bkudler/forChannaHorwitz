f7ffc8de868ac88195d4a2e1648ec128b572b36d
Ben Kudler <bkudler@digitalremedy.com>
2019-07-25 12:43:55 -0400

    fixes tests

--- chanaPy/test_grid.py
+++ chanaPy/test_grid.py
@@ -2,31 +2,42 @@
 
 import sonok
 
-def test_chana_grid():
-    maker = sonok.SonakMaker(40)
-    maker.build_grid()
-    print("HEY")
-    grid = maker.grid
-    assert grid[40][0].color == "dark green"
-    assert grid[40][0].x_pos == 8
-    assert grid[40][0].y_pos == 8
-    assert grid[40][0].present == True
+import unittest
+
+class TestChanaGrid(unittest.TestCase):
     
-    assert grid[25][1].color ==  "light blue"
-    assert grid[25][1].x_pos ==  7
-    assert grid[25][1].y_pos ==  6
-    assert grid[25][1].present ==  True
-  
-    assert grid[1][5].color ==  "orange"
-    assert grid[1][5].x_pos ==  6
-    assert grid[1][5].y_pos ==  1
-    assert grid[1][5].present ==  True
+    def setUp(self):
+        maker = sonok.SonakMaker(40)
+        maker.build_grid()
+        self.grid = maker.grid
 
-    assert grid[11][7].color ==  "yellow"
-    assert grid[11][7].x_pos ==  1
-    assert grid[11][7].y_pos ==  3
-    assert grid[11][7].present ==  True
+        
+    def test_end(self):
+        self.assertEqual( self.grid[40][0].color, "dark green")
+        self.assertEqual(self.grid[40][0].x_pos, 8)
+        self.assertEqual(self.grid[40][0].y_pos, 8)
+        self.assertEqual( self.grid[40][0].present,True)
+        
+    def test_middle(self):
+        self.assertEqual(self.grid[25][1].color,  "light blue")
+        self.assertEqual(self.grid[25][1].x_pos, 7)
+        self.assertEqual(self.grid[25][1].y_pos , 6)
+        self.assertEqual(self.grid[25][1].present, True)
+    
+    def test_beginning(self):
+        self.assertEqual(self.grid[1][5].color, "orange")
+        self.assertEqual(self.grid[1][5].x_pos, 6)
+        self.assertEqual(self.grid[1][5].y_pos, 1)
+        self.assertEqual(self.grid[1][5].present, True)
 
 
+    def test_random(self):
+        self.assertEqual(self.grid[11][7].color, "yellow")
+        self.assertEqual(self.grid[11][7].x_pos, 1)
+        self.assertEqual(self.grid[11][7].y_pos, 3)
+        self.assertEqual(self.grid[11][7].present, True)
 
-test_chana_grid();
+
+if __name__ == '__main__':
+    unittest.main()
+
